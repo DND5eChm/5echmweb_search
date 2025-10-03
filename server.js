dbb49c3a2e218cf0eba5c2c38a2ab769fe6d52f0
SilverDragon <kagangtuya@foxmail.com>
2025-10-04 01:43:44 +0800

    cors配置

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 // 中间件
-app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'OPTIONS'], allowedHeaders: ['*'] }));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
 app.use(express.static(__dirname));
 
 // 加载并解析数据文件
